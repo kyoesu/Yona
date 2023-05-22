95c339ec286994617c0619371bf522096ff2b3ca
kyoesu <roma.hells@mail.ru>
2023-05-22 22:59:19 +0400

    add gpt

---
+++ main_chat.py
@@ -0,0 +1,71 @@
+
+import numpy as np 
+#import main_chat_foo
+from main_chat_foo import *
+
+ 
+# Определение класса рекуррентной нейронной сети 
+class RecurrentNeuralNetwork: 
+    def init(self):
+    # Инициализация параметров нейросети 
+        self.weights = np.random.randn(1, 1) 
+        self.hidden_state = 0 
+
+
+    def train_unsupervised(self, data): 
+        # Обучение без учителя 
+        # Здесь выполняется алгоритм кластеризации, понижение размерности или другие методы без учителя 
+        # Данные для обучения передаются в виде массива data
+
+        # Пример обработки данных без учителя 
+        processed_data = process_unsupervised_data(data) 
+        self.weights = calculate_weights(processed_data) 
+    
+    def train_rl(self, question, answer): 
+        # Обучение с подкреплением 
+        # Здесь выполняется алгоритм обучения с подкреплением, например, с помощью Q-обучения или других методов RL 
+        # Вопрос и правильный ответ передаются в виде строк question и answer
+        # Пример обработки данных с подкреплением
+        processed_question = process_question(question) 
+        processed_answer = process_answer(answer) 
+        reward = calculate_reward(processed_question, processed_answer) 
+        
+        # Обновление весов на основе полученной награды 
+        self.weights += reward * self.hidden_state
+
+
+    def generate_text(self): 
+        # Генерация текста на основе текущих весов нейросети 
+        # Здесь выполняется алгоритм генерации текста с использованием рекуррентных связей 
+        generated_text = "" 
+        for _ in range(max_length): 
+            # Генерация следующего символа на основе текущего состояния и весов 
+            next_char = generate_next_char(self.hidden_state, self.weights)
+            
+            # Обновление скрытого состояния на основе сгенерированного символа 
+            self.hidden_state = update_hidden_state(self.hidden_state, next_char) 
+            
+            # Добавление сгенерированного символа к тексту 
+            generated_text += next_char 
+            
+        return generated_text
+
+
+# Пример использования нейросети 
+def main(): 
+    rnn = RecurrentNeuralNetwork() 
+    
+    # Обучение без учителя 
+    unsupervised_data = load_unsupervised_data() 
+    rnn.train_unsupervised(unsupervised_data) 
+    
+    # Обучение с подкреплением 
+    question = input("Введите вопрос: ")
+    answer = input("Введите ответ: ") 
+    rnn.train_rl(question, answer) 
+    
+    # Генерация текста 
+    generated_text = rnn.generate_text() 
+    print("Сгенерированный текст:", generated_text)
+if __name__ == 'main':
+    main()
--- req/main.py
+++ req/main.py
@@ -1,25 +1,52 @@
-def prop(tex):
+import pandas as pd
+
+def write(res):
+    fs = open('req/res.txt','w')
+    try:
+    # работа с файлом
+        fs.write(res)
+    finally:
+        fs.close()
+
+def prop(tex):#удаление html
     messages=[]#text,time,role
     tex2=""
-    for i in range(len(tex)-1):
+    for i in range(len(tex)-2):
         if tex[i]=="<":
             access=False
-        if access and tex[i:i+1]!="\n\n":
+        if access:# and tex[i:i+1]!="\r\n":
             tex2+=tex[i]
         if tex[i]==">":
             access=True
     #print(tex2)        
     
     return tex2
+
+def forma(tex):
+    tex1=""
+    ch=False
+    for i  in range(len(tex)):
+        if tex[i:i+25]=="ПрофильСообщенияЛаная Кит":
+            ch=True
+        if tex[i:i+2]!="\n\n"and ch:
+            tex1+=tex[i]
+        else:
+            i+=1    
+    return (tex1)
+
+
 i=0
 result=""
-while i <=1:#57750:
+while i <=2:#57750:
     file="req/lin/messages"+str(i)+".html"
     f = open (file , 'r')
     result += prop(f.read())
-
-    print(result)
+    
+    #print(result)
     f.close()
     i+=50
 
 
+#write(result)
+
+print(forma(result))
