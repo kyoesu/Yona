e83bd9bc2ceb38d2e59a11edd337eccbac44030b
kyoesu <roma.hells@mail.ru>
2023-09-21 19:34:16 +0400

    add sql funk

--- tg/base.py
+++ tg/base.py
@@ -2,27 +2,53 @@ import sqlite3
 
 
 
-
-
-def sql_k(name):
-    # Создаем подключение к базе данных (файл my_database.db будет создан)
-    connection = sqlite3.connect(name)
+def sql_begin():
+    connection = sqlite3.connect("tg/Domino.db")# Создаем подключение к базе данных (файл my_database.db будет создан)
     cursor = connection.cursor()
-    # Создаем таблицу Users
-    cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER
-)
-''')
-
-
-
-    # Сохраняем изменения и закрываем соединение
+    return cursor
+def sql_end(connection):
     connection.commit()
     connection.close()
 
+def sql_adm():
+    
+    connection = sqlite3.connect("tg/Domino.db")
+    cursor = connection.cursor()
+    
+    cursor.execute('''
+SELECT *
+FROM [Users]
+ORDER BY [id] DESC LIMIT 500
+''')
 
-sql_k("tg/Domino.db")
+
+    res=cursor.fetchall()
+    # Сохраняем изменения и закрываем соединение
+    connection.commit()
+    connection.close()
+    return res
+
+def sql_new_user(id,name):
+    connection = sqlite3.connect("tg/Domino.db")
+    cursor = connection.cursor()
+    cursor.execute(f"INSERT INTO users (name, link) VALUES ('{name}', '{id}')")
+
+    connection.commit()
+    connection.close()
+
+def sql_new_age(id,age):
+    connection = sqlite3.connect("tg/Domino.db")
+    cursor = connection.cursor()
+    cursor.execute(f"UPDATE users SET age = {age} WHERE link = '{id}';")
+
+    connection.commit()
+    connection.close()
+
+def sql_new_desk(id, desk):
+    connection = sqlite3.connect("tg/Domino.db")
+    cursor = connection.cursor()
+    cursor.execute(f"UPDATE users SET else = {desk} WHERE link = '{id}';")
+
+    connection.commit()
+    connection.close()
+#print(sql_k())
